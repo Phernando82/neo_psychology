c1674c548f997c85cb0231f4916c4f71775c8f72
Fernando Valverde <nandovalverde@gmail.com>
2022-10-11 21:25:37 +0100

    path de files, layout

--- base/apps.py
+++ base/apps.py
@@ -15,19 +15,20 @@ WIDGET_SUBTYPE_KEY = '/Widget'
 
 def write_pdf(input_pdf, output_pdf, data_dict):
     template_pdf = pdfrw.PdfReader(input_pdf)
+    for x in range(0, 10):   # PDF com 10 páginas
+        annotations = template_pdf.pages[x][ANNOT_KEY]
+        # annotations = template_pdf.pages[0][ANNOT_KEY]
+        print(type(annotations))
+        for annotation in annotations:
+            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+                if annotation[ANNOT_FIELD_KEY]:
+                    key = annotation[ANNOT_FIELD_KEY][1:-1]
+                    if key in data_dict.keys():
+                        update = {
+                            'V': data_dict[key],
+                        }
+                        annotation.update(pdfrw.PdfDict(**update))
+            annotation.update(pdfrw.PdfDict(Ff=1))
 
-    annotations = template_pdf.pages[0][ANNOT_KEY]
-
-    for annotation in annotations:
-        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-            if annotation[ANNOT_FIELD_KEY]:
-                key = annotation[ANNOT_FIELD_KEY][1:-1]
-                if key in data_dict.keys():
-                    update = {
-                        'V': data_dict[key],
-                    }
-                    annotation.update(pdfrw.PdfDict(**update))
-        annotation.update(pdfrw.PdfDict(Ff=1))
-
-    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
-    pdfrw.PdfWriter().write(output_pdf, template_pdf)
+        template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
+        pdfrw.PdfWriter().write(output_pdf, template_pdf)
